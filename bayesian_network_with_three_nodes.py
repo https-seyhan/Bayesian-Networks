4cefa0727722939b4e2ffc6b2b2201e216430de9
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2023-08-10 12:48:09 +1000

    Add files via upload

--- bayesian_network_with_three_nodes.py
+++ bayesian_network_with_three_nodes.py
@@ -3,6 +3,7 @@
 from pgmpy.models import BayesianModel
 from pgmpy.estimators import MaximumLikelihoodEstimator
 from pgmpy.inference import VariableElimination
+import pandas as pd
 
 # Define the Bayesian network structure
 model = BayesianModel([('A', 'C'), ('B', 'C')])
@@ -14,11 +15,23 @@ data = {
     'C': [0, 0, 0, 1]
 }
 
+print("Model : ", model)
+print("Type of Data ", type(data))
+print("Data ", data)
+
+# Convert dictionary to DataFrame
+data = pd.DataFrame(data)
+
 # Create a MaximumLikelihoodEstimator
 mle = MaximumLikelihoodEstimator(model, data)
 
+	
+print("mle type ", type(mle))
 # Estimate the parameters based on the data
-model.fit(data, estimator=mle)
+#model.fit(Bayes( A= [0, 0, 1, 1], B= [0, 1, 0, 1], C= [0, 0, 0, 1]), estimator=mle)
+
+#model.fit(data, estimator=mle)
+model.fit(data)
 
 # Create a VariableElimination object for inference
 inference = VariableElimination(model)
